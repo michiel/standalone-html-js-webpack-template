f143d9516740dde3a8f14bc16b44d5c7d2b12b9c
Michiel Kalkman <michiel@nosuchtype.com>
2025-01-16 08:41:01 +1100

    feat: add sample data

--- demos/random.js
+++ demos/random.js
@@ -43,3 +43,25 @@ window.visualizationDataSettings = {
 
 window.visualizationData = visData;
 
+window.graphData = {
+  nodes: [
+    { id: 1, name: 'Node 1', layer: 'Layer 1', attrs: { type: 'type1' } },
+    { id: 2, name: 'Node 2', layer: 'Layer 2', attrs: { type: 'type2' } },
+    { id: 3, name: 'Node 3', layer: 'Layer 1', attrs: { type: 'type1' } },
+    { id: 4, name: 'Node 4', layer: 'Layer 3', attrs: { type: 'type3' } },
+    { id: 5, name: 'Node 5', layer: 'Layer 2', attrs: { type: 'type2' } },
+    { id: 6, name: 'Node 6', layer: 'Layer 3', attrs: { type: 'type3' } },
+    { id: 7, name: 'Node 7', layer: 'Layer 1', attrs: { type: 'type1' } },
+    { id: 8, name: 'Node 8', layer: 'Layer 2', attrs: { type: 'type2' } }
+  ],
+  links: [
+    { source: 1, target: 2, layer: 'Layer 1', attrs: { type: 'typeA' } },
+    { source: 2, target: 3, layer: 'Layer 2', attrs: { type: 'typeB' } },
+    { source: 3, target: 4, layer: 'Layer 1', attrs: { type: 'typeA' } },
+    { source: 4, target: 5, layer: 'Layer 3', attrs: { type: 'typeC' } },
+    { source: 5, target: 6, layer: 'Layer 2', attrs: { type: 'typeB' } },
+    { source: 6, target: 7, layer: 'Layer 3', attrs: { type: 'typeC' } },
+    { source: 7, target: 8, layer: 'Layer 1', attrs: { type: 'typeA' } },
+    { source: 8, target: 1, layer: 'Layer 2', attrs: { type: 'typeB' } }
+  ]
+};
